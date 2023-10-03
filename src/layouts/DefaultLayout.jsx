bb666f545a85a55520a6d238752b228090c72810
emily40830 <emily40830@gmail.com>
2023-10-03 13:56:16 +0800

    fix: Update some UI

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,11 +1,14 @@
 import { Layout } from "antd";
 import Container from "../components/common/Container";
+import { Link } from "react-router-dom";
 
 const DefaultLayout = ({ children }) => {
   return (
     <Layout>
       <Layout.Header>
-        <h1 className="c-white text-center text-white">Simple Shop</h1>
+        <Link to="/">
+          <h1 className="c-white text-center text-white">Simple Shop</h1>
+        </Link>
       </Layout.Header>
 
       <Layout.Content className="py-5">
